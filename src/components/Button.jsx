a4703d8a393558a67169361d21c3df45052277f3
Jayan Shrestha <jayanshr44@gmail.com>
2026-02-06 06:40:36 +1100

    button update

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import ButtonSvg from "../assets/assets/svg/ButtonSvg";
 
 const Button = (props) => {
-  const classes = `button text-sm relative items-center justify-center h-11 transition-colors dark:hover:text-color-1 text-slate-900 dark:text-n-1 ${props.px || "px-7"} ${props.white ? "text-n-1" : "text-n-8"} ${props.className || ""}`;
+  const classes = `button text-sm relative items-center justify-center h-11 transition-colors dark:hover:text-color-1 text-slate-900 dark:text-n-1 ${props.px || "px-1"} ${props.white ? "text-n-1" : "text-n-8"} ${props.className || ""}`;
   const spanClasses = "relative z-10";
   const renderButton = () => (
     <button className={classes} onClick={props.onClick}>
