deed59b657bc7f1a8b6d07da42fec444daf676b2
Jayan Shrestha <jayanshr44@gmail.com>
2026-02-25 14:30:37 +1100

    updated color for name and landing page

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -19,7 +19,7 @@ const Section = ({
         <>
           <SectionSvg crossesOffset={crosses} />
           <div
-            className={`dark:hidden absolute top-0 left-7.5 right-7.5 h-0.25 dark:bg-stroke-1 bg-slate-50 ${crossesOffset && crossesOffset} pointer-events-none lg:block xl:left-10 right-10`}
+            className={`dark:hidden absolute top-0 left-7.5 right-7.5 h-0.25 dark:bg-stroke-1  ${crossesOffset && crossesOffset} pointer-events-none lg:block xl:left-10 right-10`}
           />
         </>
       )}
