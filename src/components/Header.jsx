d2880038f5ce1dfc010191ca05927e670968c2ec
Jayan Shrestha <jayanshr44@gmail.com>
2026-02-28 22:17:43 +1100

    fixed transparent header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
 
   return (
     <div
-      className={`flex fixed top-0 left-0 z-50 border-b border-slate-200 dark:border-n-6 dark:bg-n-8  w-full justify-evenly`}
+      className={`flex fixed top-0 left-0 z-50 border-b border-slate-200 dark:border-n-6 bg-n-1 dark:bg-n-8  w-full justify-evenly`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="/">
@@ -41,7 +41,7 @@ const Header = () => {
       </div>
 
       <nav
-        className={`${openNavigation ? "flex h-fit  drop-shadow-md shadow-black bg-slate-100 dark:bg-n-8/50 mt-8 translate-x-0 backdrop-blur-md" : "h-fit -translate-x-full lg:translate-x-0 mt-8"} fixed top-[5rem] left-0 right-0 bottom-0 lg:static lg:flex lg:mx-auto lg:bg-transparent transition-all duration-300 ease-in-out `}
+        className={`${openNavigation ? "flex h-fit  drop-shadow-md shadow-black  dark:bg-n-8/50 mt-8 translate-x-0 backdrop-blur-md" : "h-fit -translate-x-full lg:translate-x-0 mt-8"} fixed top-[5rem] left-0 right-0 bottom-0 lg:static lg:flex lg:mx-auto lg:bg-transparent transition-all duration-300 ease-in-out `}
       >
         <div className="relative z-2 flex flex-col items-center mx-auto mt-6 lg:flex-row">
           <a
